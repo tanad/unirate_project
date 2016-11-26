9f7f94e54edfa947e4d2dcf4a08be2df1bbef757
tanad <dragonova.tatiana@gmail.com>
2016-11-26 21:25:22 +0000

    endpoint create user added

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ require 'api_constraints'
 Workspace::Application.routes.draw do
   
   devise_for :users
-  namespace :api, defaults:  { format: :json },
-                                constraints: { subdomain: 'api' } , path: '/' do
+  namespace :api, defaults:  { format: :json } , path: '/api' do
     scope module: :v1, 
                 constraints: ApiConstraints.new(version: 1, default: true) do
-                   resources :users, :only => [:show]
+                   resources :users, :only => [:show, :create]
     end
   end
 end
