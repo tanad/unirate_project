a993780a82aad0040145c543f9c91870fff2c85b
tanad <dragonova.tatiana@gmail.com>
2016-11-28 20:43:38 +0000

    adds user authentication token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123223727) do
+ActiveRecord::Schema.define(version: 20161128162419) do
+
+  create_table "schools", force: :cascade do |t|
+    t.string   "name"
+    t.string   "country"
+    t.string   "string"
+    t.string   "city"
+    t.string   "address"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "schools", ["user_id"], name: "index_schools_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161123223727) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "auth_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
